a76f13a657b99d9b4ff63d2e5c384c93ec63ea9f
ramakrishna <letzautomate@gmail.com>
2021-03-01 22:48:28 +0530

    Common Project implementation

--- src/test/java/com/letzautomate/tests/Test001.java
+++ src/test/java/com/letzautomate/tests/Test001.java
@@ -1,7 +1,9 @@
 package com.letzautomate.tests;
 
+import com.letzautomate.pages.CommonLogin;
 import com.letzautomate.pages.application.Login;
 import com.letzautomate.utilities.TestcaseManager;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class Test001 extends TestcaseManager {
@@ -9,10 +11,14 @@ public class Test001 extends TestcaseManager {
     @Test(groups={"regression", "TC0001"})
     public void test001() {
         Login login = new Login();
+        CommonLogin commonLogin = new CommonLogin();
 
         setTestcaseName("Login Functionality");
 
         login.login();
+        commonLogin.login();
+        Actions actions;
+
     }
 
 }
